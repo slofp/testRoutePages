8b2f84f0e761461b38fb5ec2efa930204fb34cf2
Fairy-Phy <phy.public@gmail.com>
2023-11-14 18:40:47 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	import svelteLogo from "./assets/svelte.svg";
+	import viteLogo from "/vite.svg";
+	import Counter from "./lib/Counter.svelte";
+	import { Route } from "tinro";
+	import { fly } from "svelte/transition";
+</script>
+
+<main>
+	<Route path="/">
+		<div in:fly={{ x: 200 }}>
+			<div>
+				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+					<img src={viteLogo} class="logo" alt="Vite Logo" />
+				</a>
+				<a href="https://svelte.dev" target="_blank" rel="noreferrer">
+					<img
+						src={svelteLogo}
+						class="logo svelte"
+						alt="Svelte Logo"
+					/>
+				</a>
+			</div>
+			<h1>Vite + Svelte</h1>
+
+			<div class="card">
+				<Counter />
+				<a class="route-btn" href="/aaa">/aaa に移動</a>
+			</div>
+
+			<p>
+				Check out <a
+					href="https://github.com/sveltejs/kit#readme"
+					target="_blank"
+					rel="noreferrer">SvelteKit</a
+				>, the official Svelte app framework powered by Vite!
+			</p>
+
+			<p class="read-the-docs">
+				Click on the Vite and Svelte logos to learn more
+			</p>
+		</div>
+	</Route>
+	<Route path="/aaa">
+		<div in:fly={{ x: 200 }}>
+			<a class="route-btn" href="/">/ に移動</a>
+		</div>
+	</Route>
+</main>
+
+<style>
+	.logo {
+		height: 6em;
+		padding: 1.5em;
+		will-change: filter;
+		transition: filter 300ms;
+	}
+	.logo:hover {
+		filter: drop-shadow(0 0 2em #646cffaa);
+	}
+	.logo.svelte:hover {
+		filter: drop-shadow(0 0 2em #ff3e00aa);
+	}
+	.read-the-docs {
+		color: #888;
+	}
+</style>
